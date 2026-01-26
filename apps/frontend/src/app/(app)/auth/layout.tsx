16378008c1e801c562017a55759efbac54e9f41c
shahzebpyc <shahzebpyc@gmail.com>
2026-01-26 13:43:26 +0500

    chore: Update entrepreneur count to 100+ and remove the TestimonialComponent from the authentication layout.

--- apps/frontend/src/app/(app)/auth/layout.tsx
+++ apps/frontend/src/app/(app)/auth/layout.tsx
@@ -26,12 +26,11 @@ export default async function AuthLayout({
       </div>
       <div className="text-[36px] flex-1 pt-[88px] hidden lg:flex flex-col items-center">
         <div className="text-center">
-          Over <span className="text-[42px] text-[#FC69FF]">20,000+</span>{' '}
+          Over <span className="text-[42px] text-[#FC69FF]">100+</span>{' '}
           Entrepreneurs use
           <br />
           Vinsmoke Media To Grow Their Social Presence
         </div>
-        <TestimonialComponent />
       </div>
     </div>
   );
